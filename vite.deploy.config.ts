4f4adf0fccee4f37c8b7fa672605ff2c7459bc45
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2024-11-10 14:55:15 -0700

    :cloud: Publish with base url

--- vite.deploy.config.ts
+++ vite.deploy.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 defineConfig({
+  base: process.env.REPO_NAME ? `/${process.env.REPO_NAME}/` : "/",
   build: {
     rollupOptions: {
       input: "index.html",
